95106c86e8db94a419166ecc498a109b06a1cdb5
miquelmasso <miquelmassomoreno@gmail.com>
2021-09-29 16:57:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/memesity/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.memesity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnSignIn = findViewById(R.id.btnSignIn);
+        EditText txtUsername = findViewById(R.id.txtUsername);
+        EditText txtPassword = findViewById(R.id.txtPassword);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                if(txtUsername.getText().toString().equals("123") && txtPassword.getText().toString().equals("123")){
+                    Log.i("test","hola he fet click"); //comprobacio de fer click
+                }
+            }
+        });
+    }
+}
