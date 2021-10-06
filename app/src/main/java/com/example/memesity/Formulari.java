51422eb4650502aa0c97c36922521fa87798971b
miquelmasso <miquelmassomoreno@gmail.com>
2021-10-06 16:08:41 +0200

    subida 3

---
+++ app/src/main/java/com/example/memesity/Formulari.java
@@ -0,0 +1,14 @@
+package com.example.memesity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formulari extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulari);
+    }
+}
--- app/src/main/java/com/example/memesity/MainActivity.java
+++ app/src/main/java/com/example/memesity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.memesity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -8,15 +9,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
         Button btnSignIn = findViewById(R.id.btnSignIn);
         EditText txtUsername = findViewById(R.id.txtUsername);
         EditText txtPassword = findViewById(R.id.txtPassword);
+
+       // Intent goFormulari = new Intent( packageContext: this, Formulari.class);
+
+
+        BottomNavigationView bottomNav = findViewById(R.id.main_menu);
+
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new FragmentHome();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        });
+
+
+
+
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
